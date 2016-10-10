419da0a1a41037070d7bd34c85fe79250fb77d5e
Genesis Ian Palaganas <palaganas.genesis@gmail.com>
2016-10-10 22:52:10 +0800

    Add subtraction to symbolic math classes

--- symbolic_math.py
+++ symbolic_math.py
@@ -11,6 +11,8 @@ class Variable:
 
         if self.multiplier == 0:
             return '0'
+        elif self.multiplier == -1:
+            return '-' + self.name
         elif self.exponent == 0:
             return '1'
 
@@ -38,7 +40,8 @@ class Variable:
         elif isinstance(rhs,AlgebExp):
             holder = rhs.copy()
             if rhs.variables.has_key(self.get_exp_name()):
-                holder.variables[self.name] = rhs.variables[self.name] + self
+                holder.variables[self.get_exp_name()] = \
+                        rhs.variables[self.get_exp_name()] + self
             else:
                 holder.add_variable(self)
             return holder
@@ -48,6 +51,11 @@ class Variable:
             holder = holder + rhs
             return holder
 
+    def __sub__(self,rhs):
+        return self + (-rhs)
+
+    def __rsub__(self,lhs):
+        return lhs + (-self)
     
     def __radd__(self,lhs):
         return self.__add__(lhs)
@@ -74,21 +82,37 @@ class AlgebExp:
         constant = self.constant
         keys = sorted(self.variables.keys())
 
-        for i, variable in enumerate(keys):
-            if str(self.variables[variable]) == '0':
+        for i, key in enumerate(keys):
+            if str(self.variables[key]) == '0':
                 continue
-            elif str(self.variables[variable]) == '1':
+            elif str(self.variables[key]) == '1':
                 constant += 1
             else:
-                ret += str(self.variables[variable])
+                ret += str(self.variables[key])
             
                 if i < len(self.variables.keys()) - 1:
-                    ret += '+'
+                    next_key = keys[i+1]
+                    if self.variables[next_key].multiplier > 0: 
+                        ret += '+'
 
         if self.constant != 0:
-            ret += '+' + str(self.constant)
+            if self.constant > 0:
+                ret += '+'
+
+            ret += str(self.constant)
         
         return ret
+    
+    def __neg__(self):
+        holder = self.copy()
+        holder.constant = -holder.constant
+
+        for key in holder.variables.keys():
+            cur_var = holder.variables[key]
+            holder.variables[key] = \
+                    Variable(cur_var.name,-cur_var.multiplier,cur_var.exponent)
+
+        return holder
 
     def __add__(self,rhs):
         if isinstance(rhs,Real):
@@ -120,6 +144,12 @@ class AlgebExp:
 
             return holder
     
+    def __sub__(self,rhs):
+        return self + (-rhs)
+
+    def __rsub__(self,lhs):
+        return lhs + (-self)
+    
     def add_variable(self,variable):
         if variable.get_exp_name() not in self.variables.keys():
             self.variables[variable.get_exp_name()] = variable
@@ -138,13 +168,7 @@ exp = x + x1
 y = Variable('y')
 exp2 = x + y
 exp3 = x + x
-
-print exp2.variables.keys()
+exp4 = x + y + 3
 
 print exp
-print exp2
-print exp3
-print exp + exp2
-print exp + exp3
-print exp2 + exp3
-print exp + exp2 + exp3
+print exp - Variable('x',1,2)
